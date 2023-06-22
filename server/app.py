eebeca38732e22a1c5fd9375f7fe7b11eb52eceb
richard miruka onsare <richardonsare15@gmail.com>
2023-06-22 19:46:50 +0300

    set up a hook so that our views all know where our application files are located

--- server/app.py
+++ server/app.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request
+import os
+
+from flask import Flask, request, current_app, g 
 
 app = Flask(__name__)
+
+@app.before_request
+def app_path():
+    g.path = os.path.abspath(os.getcwd())
 
 @app.route('/')
 def index():
     host = request.headers.get('Host')
-    return f'<h1>The host of this page is {host}</h1>'
-
+    appname = current_app.name
+    return f'''<h1>The host of this page is {host}</h1>'
+               <h2>The name of this application is {appname}</h2>
+               <h3> The path of this apllication on the user's device is {g.path}</h3>'''
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5555, debug=True) 
